5f36352ce752798364a1b1a5b42fc86bab63e044
1587579553 <79459552+1587579553@users.noreply.github.com>
2022-09-25 16:19:36 +0800

    upload github_task_one

---
+++ git_demo_one.py
@@ -0,0 +1,7 @@
+for i in range(10):
+    print("I love python more than 3000 times")
+    print("I dont't konw what I am doing now.")
+    print("can you tell me why the teacher taught me is easy,but we learn is difficult")
+    print("I am tire,pls let me fly from the tallest floor")
+    print("today is wendsday")
+    print("today is sunday!I fell so ")
