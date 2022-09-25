6cd70ddd109f33e861c1d13a89085805786ce3c1
954637773 <114070817+954637773@users.noreply.github.com>
2022-09-25 17:03:18 +0800

    update github_task_one
    团队成员支持github在线开发

--- git_demo_one.py
+++ git_demo_one.py
@@ -4,4 +4,4 @@ for i in range(10):
     print("can you tell me why the teacher taught me is easy,but we learn is difficult")
     print("I am tire,pls let me fly from the tallest floor")
     print("today is wendsday")
-    print("today is sunday!I fell so ")
+    print("today is sunday!I fell so sorrowful！wuwuwu")
